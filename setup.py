9fd4914b0324c5495a3e40ff88a060652d27dcfd
Dan Roberts <dan@dan-desktop.(none)>
2008-11-21 04:04:59 -0800

    Added license information, the entire project is licensed under the GNU GPL Version 2.

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #! /usr/bin/env python
 
+# Copyright 2008 Daniel Roberts
+# This file is part of Media Mouse.
+# 
+# Media Mouse is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+# 
+# Media Mouse is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Media Mouse.  If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 from os.path import join
 
--- src/media_mouse/banshee.py
+++ src/media_mouse/banshee.py
@@ -1,3 +1,18 @@
+# Copyright 2008 Daniel Roberts
+# This file is part of Media Mouse.
+# 
+# Media Mouse is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+# 
+# Media Mouse is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Media Mouse.  If not, see <http://www.gnu.org/licenses/>.
+
 class Banshee(object):
 	bus_name = "org.bansheeproject.Banshee"
 	playback_iface = "/org/bansheeproject/Banshee/PlaybackController"
